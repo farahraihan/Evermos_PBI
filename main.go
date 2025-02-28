33767eb0e7bce9c521a52bd3e2d9094256f594d0
farahraihan <raihanunnisafarah@gmail.com>
2025-02-28 12:02:32 +0700

    Fix port for Vercel deployment

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"evermos_pbi/internal/factory"
+	"net/http"
 	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
+// Handler untuk Vercel
+func Handler(w http.ResponseWriter, r *http.Request) {
+	e := echo.New()
+	factory.InitFactory(e)
+	e.ServeHTTP(w, r)
+}
+
 func main() {
 	e := echo.New()
-
 	factory.InitFactory(e)
 
-	// Gunakan port dari environment (default ke 3000 untuk Vercel)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
