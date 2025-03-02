1b21d88939cac245af8c296225cf9056abefb224
farahraihan <raihanunnisafarah@gmail.com>
2025-03-02 21:38:39 +0700

    update vercel config

--- main.go
+++ main.go
@@ -2,27 +2,14 @@ package main
 
 import (
 	"evermos_pbi/internal/factory"
-	"net/http"
-	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
-// Handler untuk Vercel
-func Handler(w http.ResponseWriter, r *http.Request) {
-	e := echo.New()
-	factory.InitFactory(e)
-	e.ServeHTTP(w, r)
-}
-
 func main() {
 	e := echo.New()
+
 	factory.InitFactory(e)
+	e.Logger.Error(e.Start(":8000"))
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
-
-	e.Logger.Fatal(e.Start(":" + port))
 }
