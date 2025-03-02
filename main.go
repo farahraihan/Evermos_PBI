eb2a35564464469b44b54fe431119bd0f2793504
farahraihan <raihanunnisafarah@gmail.com>
2025-03-02 22:04:16 +0700

    update vercel config

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
-package handler
+// package handler
+
+// import (
+// 	"evermos_pbi/internal/factory"
+// 	"net/http"
+
+// 	"github.com/labstack/echo/v4"
+// )
+
+// func Handler(w http.ResponseWriter, r *http.Request) {
+// 	e := echo.New()
+// 	factory.InitFactory(e)
+// 	e.ServeHTTP(w, r)
+// }
+
+package main
 
 import (
 	"evermos_pbi/internal/factory"
-	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func main() {
 	e := echo.New()
+
 	factory.InitFactory(e)
-	e.ServeHTTP(w, r)
+	e.Logger.Error(e.Start(":8000"))
+
 }
