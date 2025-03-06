75cf3f6bbe7521bbed758d0fa9de77448afc7f79
farahraihan <raihanunnisafarah@gmail.com>
2025-03-06 09:44:23 +0700

    config update

--- main.go
+++ main.go
@@ -1,18 +1,3 @@
-// package handler
-
-// import (
-// 	"evermos_pbi/internal/factory"
-// 	"net/http"
-
-// 	"github.com/labstack/echo/v4"
-// )
-
-// func Handler(w http.ResponseWriter, r *http.Request) {
-// 	e := echo.New()
-// 	factory.InitFactory(e)
-// 	e.ServeHTTP(w, r)
-// }
-
 package main
 
 import (
